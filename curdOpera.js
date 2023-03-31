52f174f75e578f3944ced884bb1be6dbf52025a4
ShaileshDhoot <128277965+ShaileshDhoot@users.noreply.github.com>
2023-03-31 06:20:28 +0530

    Add files via upload

--- curdOpera.js
+++ curdOpera.js
@@ -15,6 +15,7 @@ form.addEventListener('submit', (e) => {
     // localStorage.setItem(personData.email, JSON.stringify(personData))
     // const displayPersonData = JSON.parse(localStorage.getItem(personData.email))
     // console.log(displayPersonData);
+
     const newLi = document.createElement('li')
     newLi.className = "newItem "
     newLi.textContent = `${name}     ${mobile}     ${email}`
@@ -32,13 +33,44 @@ form.addEventListener('submit', (e) => {
     newLi.appendChild(del)
     newLi.appendChild(edit)
 
+
     document.querySelector("#name").value = ""
     document.querySelector("#mobile").value = ""
     document.querySelector('#email').value = ""
 
+    // newLi.addEventListener('click', (e) => {
+    //     if (e.target.classList.contains('delete')) {
+    //         axios.delete(`'https://crudcrud.com/api/c2bf44aa787142ca906a26200f4bb12b/Register/${_id}'`)
+    //             .then(response => showOutput(res))
+    //             .catch(err => console.error(err))
+    //     }
+        // if (e.target.classList.contains('edit')) {
+        //     document.querySelector("#name").value = name
+        //     document.querySelector("#mobile").value = mobile
+        //     document.querySelector('#email').value = email
+        //     e.target.parentNode.remove()
+        //     localStorage.removeItem(personData.email)
+        // }
+    //})
+
+    // newLi.addEventListener('click', (e) => {
+    //     if (e.target.classList.contains('delete')) {
+    //         e.target.parentNode.remove()
+    //         localStorage.removeItem(personData.email)
+    //     }
+
+    //     if (e.target.classList.contains('edit')) {
+    //         document.querySelector("#name").value = name
+    //         document.querySelector("#mobile").value = mobile
+    //         document.querySelector('#email').value = email
+    //         e.target.parentNode.remove()
+    //         localStorage.removeItem(personData.email)
+    //     }
+    // })
+
     // const config = {
     //     method: "POST",
-    //     url: 'https://crudcrud.com/api/644c82c012ff4aa88bda54c6f01a011a/appointmentData',
+    //     url: 'https://crudcrud.com/api/c2bf44aa787142ca906a26200f4bb12b/Register',
 
     //     headers: {
     //         'Content-Type': 'application/json'
@@ -60,7 +92,7 @@ form.addEventListener('submit', (e) => {
 
     //-----------display data in browser
 
-    // const res = axios('https://crudcrud.com/api/644c82c012ff4aa88bda54c6f01a011a/appointmentData')
+    // const res = axios('https://crudcrud.com/api/c2bf44aa787142ca906a26200f4bb12b/Register')
     //     .then((res) => {
     //         console.log(res.data)
     //         let output = JSON.stringify(res.data) 
@@ -71,43 +103,39 @@ form.addEventListener('submit', (e) => {
     //     })
 
     //--------using loop 
-    
-    axios('https://crudcrud.com/api/644c82c012ff4aa88bda54c6f01a011a/appointmentData')
-        .then((response) => {
-            console.log(response.data)
-            response.data.forEach(element => {
-                document.getElementById('display').innerHTML += `<div> ${element.name}  ${element.mobile} ${element.email} <button class=" w3-btn w3-blue btn divDel">DELETE</button><button class="divEdit w3-btn w3-blue btn">EDIT</button></div> <hr>` 
-            });
-            // newLi.addEventListener('click', (e) => {
-            //     if (e.target.classList.contains('divDel')) {
-            //         e.target.remove()
-            //     }
-        
-            //     if (e.target.classList.contains('divEdit')) {
-            //         document.querySelector("#name").value = name
-            //         document.querySelector("#mobile").value = mobile
-            //         document.querySelector('#email').value = email
-            //         e.target.remove()
-            //     }
-            // })
-        })
 
 
-
-
-    newLi.addEventListener('click', (e) => {
-        if (e.target.classList.contains('delete')) {
-            e.target.parentNode.remove()
-            localStorage.removeItem(personData.email)
-        }
-
-        if (e.target.classList.contains('edit')) {
-            document.querySelector("#name").value = name
-            document.querySelector("#mobile").value = mobile
-            document.querySelector('#email').value = email
-            e.target.parentNode.remove()
-            localStorage.removeItem(personData.email)
-        }
-    })
 })
+window.addEventListener("DOMContentLoaded", (e) => {
+    e.preventDefault()
+
+    axios('https://crudcrud.com/api/c2bf44aa787142ca906a26200f4bb12b/Register')
+        .then((response) => {
+            console.log(response);
+            console.log(response.data)
+
+            for (let i = 0; i < response.data.length; i++) {
+                let newUl = document.querySelector('.display')
+                const newLi = document.createElement('li')
+                newLi.className = "newItem "
+                newLi.textContent = `${response.data[i].Name}     ${response.data[i].MobileNo}     ${response.data[i].Email}`
+                const edit = document.createElement('button')
+                edit.className = "edit w3-btn w3-blue btn"
+                edit.name = "edit"
+                edit.textContent = 'Edit'
+                const del = document.createElement('button')
+                del.className = "delete w3-btn w3-blue btn"
+                del.textContent = 'Delete'
+                del.name = 'delete'
+                newUl.appendChild(newLi)
+                newLi.appendChild(del)
+                newLi.appendChild(edit)
+            };        
+        }).catch((error) => {
+            console.log(error);
+    })     
+})
+
+
+
 
